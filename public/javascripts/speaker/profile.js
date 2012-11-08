5f2e3620b48ce82cd2e0e05a445f457dca89c752
Brandon Satrom <bsatrom+gh@gmail.com>
2012-11-08 13:54:01 -0600

    demos complete

--- public/javascripts/speaker/profile.js
+++ public/javascripts/speaker/profile.js
@@ -7,4 +7,15 @@
 	$('#languages').kendoComboBox({
 		dataSource: langs
 	})
+
+	// (kd8) Add click-handler for RTL button
+	$('#toggleRTL').on('click', function(event) {
+		var form = $('#speakerForm');
+
+		if (form.hasClass('k-rtl')) {
+			form.removeClass('k-rtl')
+		} else {
+			form.addClass('k-rtl');
+		}		
+	});
 })();
